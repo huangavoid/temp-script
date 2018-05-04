5695a60f7d66a251c1ecbb1729797e0f7110df18
huangavoid <huangavoid@126.com>
2018-05-04 12:37:10 +0800

    modified script about tomcat

--- tomcat/build_instance.sh
+++ tomcat/build_instance.sh
@@ -4,13 +4,18 @@ CURR_DIR=`dirname ${BASH_SOURCE}`
 CATALINA_HOME=/opt/apache-tomcat
 APP_DIR=/srv/tomcat-webapps
 
-for i in {1..3}
-do
-    INST_DIR=${CURR_DIR}/tomcat-${i}
-    mkdir -p ${INST_DIR}/{conf,logs,work,temp}
-    cp -a ${CATALINA_HOME}/conf/* ${INST_DIR}/conf
-    sed -i  "22s@8005@8${i}05@" ${INST_DIR}/conf/server.xml
-    sed -i  "69s@8080@8${i}80@" ${INST_DIR}/conf/server.xml
-    sed -i "116s@8009@8${i}09@" ${INST_DIR}/conf/server.xml
-    sed -i "148s@appBase=\"webapps\"@appBase=\"${APP_DIR}\"@" ${INST_DIR}/conf/server.xml
-done
+if [ ${1} ]
+then
+    for i in $( seq 1 ${1} )
+    do
+        INST_DIR=${CURR_DIR}/tomcat-${i}
+        mkdir -p ${INST_DIR}/{conf,logs,work,temp}
+        cp -a ${CATALINA_HOME}/conf/* ${INST_DIR}/conf
+        sed -i  "22s@8005@8${i}05@" ${INST_DIR}/conf/server.xml
+        sed -i  "69s@8080@8${i}80@" ${INST_DIR}/conf/server.xml
+        sed -i "116s@8009@8${i}09@" ${INST_DIR}/conf/server.xml
+        sed -i "148s@appBase=\"webapps\"@appBase=\"${APP_DIR}\"@" ${INST_DIR}/conf/server.xml
+    done
+else 
+    echo "USAGE: ${0} { 1 | 2 | 3 ... }"
+fi
